e790fc092c05cc072fb363df39f2194a92415159
shibani-sahu <sibanisahu29102001@gmail.com>
2025-04-26 10:11:55 +0530

    creating templates

--- swiggy/settings.py
+++ swiggy/settings.py
@@ -14,7 +14,12 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR='D:\Github projects\DJANGO_CLS\Scripts\swiggy\templates\first.html'
+#TEMPLATES_DIR='D:\\Github projects\\DJANGO_CLS\\Scripts\\swiggy\\templates\\first.html'
+
+TEMPLATES_DIR= BASE_DIR / 'templates'
+# import os
+# TEMPLATES_DIR= 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'swiggy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES_DIR'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
